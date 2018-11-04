df96d931866188a874e1c10aa52543357482ffb9
Christian Stein <sormuras@gmail.com>
2018-11-04 12:03:20 +0100

    Add throwing main program integration test

--- src/test/java/integration/MaingineIntegrationTests.java
+++ src/test/java/integration/MaingineIntegrationTests.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.engine.discovery.DiscoverySelectors;
 import org.junit.platform.testkit.engine.ExecutionRecorder;
 import org.junit.platform.testkit.engine.ExecutionResults;
@@ -29,8 +30,8 @@ class MaingineIntegrationTests {
      *  ExecutionEvent [type = FINISHED, testDescriptor = MainClass: [engine:brahms-maingine]/[main-class:integration.MaingineIntegrationTests$SuccessfulMainProgram], timestamp = 2018-11-04T07:07:26.131277500Z, payload = TestExecutionResult [status = SUCCESSFUL, throwable = null]]
      *  ExecutionEvent [type = FINISHED, testDescriptor = EngineDescriptor: [engine:brahms-maingine], timestamp = 2018-11-04T07:07:26.131277500Z, payload = TestExecutionResult [status = SUCCESSFUL, throwable = null]]
      */
-    executionResults.all().assertStatistics(that -> that.started(3).finished(3).succeeded(3));
-    executionResults.tests().assertStatistics(that -> that.started(1).finished(1).succeeded(1));
+    executionResults.all().assertStatistics(it -> it.started(3).finished(3).succeeded(3));
+    executionResults.tests().assertStatistics(it -> it.started(1).finished(1).succeeded(1));
 
     var testDescriptor = executionResults.tests().succeeded().list().get(0).getTestDescriptor();
     assertAll(
@@ -39,14 +40,37 @@ class MaingineIntegrationTests {
         () -> assertEquals(Set.of(), testDescriptor.getTags()));
   }
 
+  @Test
+  void execute_throwing_Java_program() {
+    var executionResults = execute(ThrowingMainProgram.class);
+
+    executionResults.all().assertStatistics(it -> it.started(3).finished(3).succeeded(2));
+    executionResults.tests().assertStatistics(it -> it.started(1).finished(1).failed(1));
+
+    var testResult =
+        executionResults
+            .tests()
+            .failed()
+            .list()
+            .get(0)
+            .getRequiredPayload(TestExecutionResult.class);
+    var throwable = testResult.getThrowable().orElseThrow();
+    assertEquals("java.lang.reflect.InvocationTargetException", throwable.toString());
+    assertEquals("java.lang.Error: Guru Meditation", String.valueOf(throwable.getCause()));
+  }
+
   private ExecutionResults execute(Class<?> program) {
     var discoveryRequest = request().selectors(DiscoverySelectors.selectClass(program)).build();
     return ExecutionRecorder.execute(new MainTestEngine(), discoveryRequest);
   }
 
   public static class SuccessfulMainProgram {
+    public static void main(String[] args) {}
+  }
+
+  public static class ThrowingMainProgram {
     public static void main(String[] args) {
-      // empty
+      throw new Error("Guru Meditation");
     }
   }
 }
